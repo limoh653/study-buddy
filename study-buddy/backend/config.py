f447df5795abdacc7e3ba45fe592e8680c9c2983
Hesbon <limohesbon7@gmail.com>
2025-09-24 21:23:51 +0300

    initial commit

--- study-buddy/backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'mysecret')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///studybuddy.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwt-secret')
